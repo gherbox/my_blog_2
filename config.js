30c9a65839fc6434bfc3dc49df87daf48117c11b
Admin <admin@MacBook-Pro.local>
2020-05-09 00:04:36 -0500

    resolved issue test

--- config.js
+++ config.js
@@ -3,8 +3,8 @@
 module.exports = {
   url: 'https://lumen.netlify.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Blog by Amine Gherbi',
+  subtitle: 'Currently working as a Machine Learning engineer, with a focus on HPC and distributed deep learning.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
@@ -25,9 +25,9 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Amine Gherbi',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'Currently working as a Machine Learning engineer, with a focus on HPC and distributed deep learning.',
     contacts: {
       email: '',
       facebook: '#',
