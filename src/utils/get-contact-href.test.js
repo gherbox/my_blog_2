f7fec5f1fe187b0fd6103b38b4a65de7fc6d53c3
Admin <admin@MacBook-Pro.local>
2020-05-10 15:56:12 -0500

    add github

--- src/utils/get-contact-href.test.js
+++ src/utils/get-contact-href.test.js
@@ -2,7 +2,7 @@
 import getContactHref from './get-contact-href';
 
 test('getContactHref', () => {
-  expect(getContactHref('github', '#')).toBe('https://github.com/gherbox/#');
+  expect(getContactHref('github', '#')).toBe('#');
   expect(getContactHref('email', '#')).toBe('mailto:#');
   expect(getContactHref('linkedin', '#')).toBe('https://www.linkedin.com/amine-gherbi/#');
 });
