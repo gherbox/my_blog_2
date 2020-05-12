50a419cc4f8be9d544e8903c4b3ad8cc53bd8af3
Admin <admin@MacBook-Pro.local>
2020-05-11 22:30:42 -0500

    some changes 6

--- src/templates/tags-list-template.test.js
+++ src/templates/tags-list-template.test.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import { useStaticQuery, StaticQuery } from 'gatsby';
+import TagsListTemplate from './tags-list-template';
 import siteMetadata from '../../jest/__fixtures__/site-metadata';
 import allMarkdownRemark from '../../jest/__fixtures__/all-markdown-remark';
 import type { RenderCallback } from '../types';
