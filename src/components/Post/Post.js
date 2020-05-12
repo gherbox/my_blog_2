4826818b4c8b85bab46c0bfebdd2b48a1f56ae08
Admin <admin@MacBook-Pro.local>
2020-05-11 22:06:58 -0500

    some changes 3

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -15,8 +15,8 @@ type Props = {
 
 const Post = ({ post }: Props) => {
   const { html } = post;
-  const { tagSlugs, slug } = post.fields;
-  const { tags, title, date } = post.frontmatter;
+  const { slug } = post.fields;
+  const { title, date } = post.frontmatter;
 
   return (
     <div className={styles['post']}>
